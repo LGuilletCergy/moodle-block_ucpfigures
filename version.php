f0abde2c67752fad5ee6629882da65ba1800862a
superviseur <superviseur@POR2-HI-lguille.administration.adm>
2018-10-09 15:24:34 +0200

    Version bump

--- version.php
+++ version.php
@@ -34,5 +34,5 @@
  */
 
 $plugin->component = 'block_ucpfigures';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
-$plugin->version = 2018100902;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2018100903;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires   = 2014051200; // Moodle 2.7
