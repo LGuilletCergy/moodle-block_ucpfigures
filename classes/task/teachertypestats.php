8bed8522fb3ea20a712127ffdbc87c9668f63d2f
LGuillet <laurent.guillet@u-cergy.fr>
2019-03-01 11:14:22 +0100

    Teacher Type statistics

--- classes/task/teachertypestats.php
+++ classes/task/teachertypestats.php
@@ -41,6 +41,56 @@ class teachertypestats extends \core\task\adhoc_task {
 
     public function execute() {
 
-        
+        global $DB;
+
+        $DB->delete_records('ucpfigures_teachertype');
+
+        $xmldocteachers = new \DOMDocument();
+        $xmldocteachers->load('/home/referentiel/DOKEOS_Enseignants_Affectations.xml');
+        $xpathvarteachers = new \Domxpath($xmldocteachers);
+        $listteachers = $xpathvarteachers->query('//Teacher');
+
+        $timestatbeginningtemp = strptime('01/07/' . $CFG->thisyear, '%d/%m/%Y');
+        $timestatbeginning = mktime(0, 0, 0, $timestatbeginningtemp['tm_mon'] + 1,
+                $timestatbeginningtemp['tm_mday'], $timestatbeginningtemp['tm_year'] + 1900);
+
+        $roleteacherid = $DB->get_record('role', array('shortname' => 'editingteacher'))->id;
+
+        foreach ($listteachers as $teacher) {
+
+            $hascourse = 0;
+
+            $teacherlogin = $teacher->getAttribute('StaffUID');
+
+            if ($DB->record_exists('user', array('username' => $teacherlogin))) {
+
+                $teacherrecord = $DB->get_record('user', array('username' => $teacherlogin));
+
+                if ($DB->record_exist('role_assignments', array('roleid' => $roleteacherid,
+                    'userid' => $teacherrecord->id, 'timemodified' => $timestatbeginning))) {
+
+                    $hascourse = 1;
+                }
+
+                if ($DB->record_exists('ucpfigures_teachertype',
+                        array('teachertype' => $teacher->getAttribute('LC_CORPS')))) {
+
+                    $teachertyperecord = $DB->get_record('ucpfigures_teachertype',
+                        array('teachertype' => $teacher->getAttribute('LC_CORPS')));
+                    $teachertyperecord->coursecreated += $hascourse;
+                    $teachertyperecord->totalusers++;
+
+                    $DB->update_record('ucpfigures_teachertype', $teachertyperecord);
+                } else {
+
+                    $teachertyperecord = new \stdClass();
+                    $teachertyperecord->teachertype = $teacher->getAttribute('LC_CORPS');
+                    $teachertyperecord->coursecreated = $hascourse;
+                    $teachertyperecord->totalusers = 1;
+
+                    $DB->insert_record('ucpfigures_teachertype', $teachertyperecord);
+                }
+            }
+        }
     }
 }
